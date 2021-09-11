84765b28974ec0a280ab25659d9223f207cf3460
Josh <joshneilgonzales@gmail.com>
2021-09-11 16:06:49 -0500

    consolidated `prng` functions

--- src/utils.js
+++ src/utils.js
@@ -9,8 +9,34 @@ export class prng {
     static int32() {
         return this._prng.int32();
     }
+    static int16() {
+        return this._prng.int32() % (2**16);
+    }
     static quick() {
         return this._prng.quick();
+    }
+    static range(min, max) {
+        min = (min !== undefined) ? min : 0;
+        max = (max !== undefined) ? max : 1;
+        return Math.floor(this._prng.quick() * (max - min + 1)) + min;
+    }
+    static choose([min, max], num) {
+        let values = [];
+        num = (num !== undefined) ? num : this.range(min, max);
+        for(let i = 0; i < num; i++) {
+            let val;
+            while(values.includes(val = this.range(min, max)));
+            values.push(val);
+        }
+        return values;
+    }
+    // Algorithm based on:
+    // Weighted Random Sampling (2005; Efraimidis, Spirakis)
+    //
+    // Credits: https://github.com/denizdogan/weighted-shuffle by denizdogan
+    static shuffle(array) {
+        return array.map(([i, w]) => [i, this.quick() ** (1 / w)])
+                    .sort((a, b) => b[1] - a[1]);
     }
     static destroy() {
         this._prng = {};
@@ -19,32 +45,4 @@ export class prng {
 
 export async function json(path) {
     return JSON.parse(await fs.readFile(path, 'utf8'));
-}
-
-// Algorithm based on:
-// Weighted Random Sampling (2005; Efraimidis, Spirakis)
-//
-// Credits: https://github.com/denizdogan/weighted-shuffle by denizdogan
-export function weightedShuffle(array) {
-    let ret = Object.assign([], array);
-    ret.forEach(item => item[1] = prng.quick() ** (1 / item[1]));
-    ret.sort((a, b) => b[1] - a[1]);
-    return ret;
-}
-
-export function rollDice(min, max) {
-    min = (min !== undefined) ? min : 0;
-    max = (max !== undefined) ? max : 1;
-    return Math.floor(prng.quick() * (max - min + 1)) + min; //inclusive range
-}
-
-export function chooseFromRange([min, max], num) {
-    let values = [];
-    num = (num !== undefined) ? num : rollDice(min, max);
-    for(let i = 0; i < num; i++) {
-        let val;
-        while(values.includes(val = rollDice(min, max)));
-        values.push(val);
-    }
-    return values;
 }
