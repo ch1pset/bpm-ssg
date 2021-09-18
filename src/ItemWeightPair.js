ee3bec70b7956ac5254e375c152b690d91cba58e
Josh <joshneilgonzales@gmail.com>
2021-09-18 04:41:16 -0500

    Handle more item generation in `ItemWeightPair`

--- src/ItemWeightPair.js
+++ src/ItemWeightPair.js
@@ -4,17 +4,18 @@ import {
 } from 'uesavetool'
 
 import {
-
+    deep_copy_template,
+    ITEMS
 } from './index.js'
 
 export class ItemWeightPair extends Tuple {
-    constructor() {
+    constructor(type) {
         super()
-        this.Name = "ItemWeightPair",
+        this.Name = `BPM${type==='Weapon'? type : 'Ability'}WeightPair`,
         this.Type = "Tuple",
         this.Properties = [
             {
-                "Name": "Item",
+                "Name": `BPM${type==='Weapon'? type : 'Ability'}\0`,
                 "Type": "SoftObjectProperty\u0000",
                 "Property": ""
             },
@@ -25,11 +26,33 @@ export class ItemWeightPair extends Tuple {
             }
         ]
     }
+    set Property([item, weight]) {
+        this.Item = item;
+        this.Weight = weight;
+    }
+    get Property() {
+        return [this.Item, this.Weight];
+    }
     set Item(value) {
         this.Properties[0].Property = value;
     }
+    get Item() {
+        let [name, prop] = deep_copy_template(ITEMS.find(([n, i]) => i.Value === this.Properties[0].Property));
+        return name;
+    }
     set Weight(value) {
         this.Properties[1].Property = [0, value];
+    }
+    get Weight() {
+        return this.Properties[1].Property[1];
+    }
+    static fromPair([name, weight]) {
+        let [item, prop] = deep_copy_template(ITEMS.find(([n, i]) => n === name));
+        if(item === undefined)
+            console.log(`Mislabled/Missing Item '${name}'`);
+        let item_weight_pair = new ItemWeightPair(prop.Type);
+        item_weight_pair.Property = [prop.Value, weight];
+        return item_weight_pair;
     }
     static from(obj) {
         let ret = new ItemWeightPair();
