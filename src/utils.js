f0cdde74516f86c23706d7d972909ab81d7d81a6
Josh <joshneilgonzales@gmail.com>
2021-09-18 00:35:03 -0500

    Add `range()` fold to `/utils`

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,11 @@
 export function deep_copy_template(template) {
     return JSON.parse(JSON.stringify(template));
 }
+
+export function range([min, max], cb) { //inclusive
+    let numbers = []
+    for(let i = min; i <= max; i++) {
+        numbers.push(cb ? cb(i) : i);
+    }
+    return numbers;
+}
