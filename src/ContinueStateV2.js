b9b0ed35cdeedcda9cdedc98ddd4dbf613c4e190
Josh <joshneilgonzales@gmail.com>
2021-09-21 22:19:38 -0500

    Remove starting weapon from weapon pools

--- src/ContinueStateV2.js
+++ src/ContinueStateV2.js
@@ -9,7 +9,8 @@ import {
     Prng,
     T_GVAS_HEADER,
     RunPropertyStore, 
-    CharacterPropertyStore 
+    CharacterPropertyStore, 
+    REQUIREMENTS
 } from './index.js';
 
 export class ContinueStateV2 extends Gvas {
@@ -18,18 +19,26 @@ export class ContinueStateV2 extends Gvas {
         this.Header = GvasHeader.from(T_GVAS_HEADER);
     }
     get Slot() {
-        return PropertyFactory.create(SAVESLOT.ContinueStateV2);
+        return SAVESLOT.ContinueStateV2;
     }
     addProperty(prop) {
         this.Properties
-            .Properties.push(prop);
+            .Properties.push(PropertyFactory.create(prop));
     }
     static seededGen({seed, char, diff, opts}) {
         let save = new ContinueStateV2();
         save.addProperty(save.Slot);
         Prng.init(seed);
-        save.addProperty(CharacterPropertyStore.generate(seed, char, opts));
-        save.addProperty(RunPropertyStore.generate(seed, diff, opts));
+        let charstore = CharacterPropertyStore.generate(seed, char, opts);
+        save.addProperty(charstore);
+        let runstore = RunPropertyStore.generate(seed, diff, opts);
+        REQUIREMENTS.POOLS.forEach(([name, type]) => {
+            if(type === 'Weapon') {
+                runstore.getProperty(`${name}\0`).delItemByValue(charstore.StartingWeapon)
+            }
+        });
+        save.addProperty(runstore);
+        console.log(save);
         Prng.destroy();
         return save;
     }
