a9f08c03736066350bf4dc8737b59885eb53d9bd
Josh <joshneilgonzales@gmail.com>
2021-09-28 13:14:49 -0500

    Moved `Generate.rooms()` to `BPMRoom.generate()`

--- src/BPMRoom.js
+++ src/BPMRoom.js
@@ -1,4 +1,16 @@
 import { IntProperty } from "uesavetool";
+import {
+    Prng,
+    range
+} from './index.js'
+
+const FLOOR = {
+    ASG_I:  0, ASG_II: 1,
+    VAN_I:  2, VAN_II: 3,
+    SVAR_I: 4, SVAR_II:5,
+    HELL_I: 6, HELL_II:7,
+    UNKOWN:8, MAX:9
+}
 
 export class BPMRoom extends IntProperty {
     constructor(name) {
@@ -17,5 +29,38 @@ export class BPMRoom extends IntProperty {
         let room = new BPMRoom(name);
         room.Floor = floor;
         return room;
+    }
+    static generate([room, rate]) {
+        switch(room) {
+            case "LibraryBeforeArmoury":
+                return range([0, 4], i => Prng.chance(rate) ?
+                    [ BPMRoom.create(2*i, room), BPMRoom.create(2*i+1, room) ] 
+                    : null).flat();
+            case "MinibossChoice":
+                return range([0, 4], i => Prng.pick(2*i, 2*i+1))
+                    .map(f => BPMRoom.create(f, room));
+            case "StairsRoomChosen":
+                return Prng.chance(rate) ?
+                    BPMRoom.create(FLOOR.ASG_I, room) : null;
+            case "ChoiceRoomChosen":
+                return Prng.chance(rate) ?
+                    BPMRoom.create(Prng.pick(FLOOR.ASG_I, FLOOR.SVAR_II), room) : null;
+            case "PortalRoomChosen":
+                return Prng.choose([FLOOR.ASG_I, FLOOR.HELL_I], Prng.chance(rate))
+                    .map(f => BPMRoom.create(f, room));
+            case "PrestigeRoomChosen":
+                return Prng.choose([FLOOR.VAN_I, FLOOR.VAN_I], Prng.chance(rate))
+                    .map(f => BPMRoom.create(f, room));
+            case "BankRoomChosen":
+            case "HeroRoomChosen":
+            case "BlackMarketRoomChosen":
+            case "RerollRoomChosen":
+            case "GamblingRoomChosen":
+                let num = 0;
+                while(Prng.chance(rate) && num++ < FLOOR.HELL_I)
+                    rate /= 2;
+                return Prng.choose([FLOOR.ASG_I, FLOOR.SVAR_II], num)
+                    .map(f => BPMRoom.create(f, room));
+        }
     }
 }
